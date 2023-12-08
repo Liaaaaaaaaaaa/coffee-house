de86866abdf57b145f6b157dedb6d01eba974cb1
Liaaaaaaaaaaa <yllia_vladimirovna@mail.ru>
2023-12-08 23:52:02 +0300

    feat: add footer

---
+++ coffee-house/src/Footer/index.js
@@ -0,0 +1,6 @@
+import Footer from "./index.html";
+import htmlToElement from "../utils/htmlToElement";
+
+const footer = htmlToElement(Footer);
+
+export default footer ;
--- coffee-house/src/Home/index.js
+++ coffee-house/src/Home/index.js
@@ -7,16 +7,20 @@ import Enjoy from '../Home/enjoy';
 import Choose from '../Home/choose';
 import Resource from './resource';
 import Download from './download';
+import Footer from '../Footer';
 
 
 const header = document.getElementById('header');
 header.append(Header);
 
-
 const root = document.getElementById('root');
+console.log(root);
 root.append(Enjoy);
 root.append(Choose);
 root.append(Resource);
 root.append(Download);
+
+const footer = document.getElementById('footer');
+footer.append(Footer);
 
 
